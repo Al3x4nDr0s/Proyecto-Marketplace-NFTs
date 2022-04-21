da200e38df2d357a4f904de3eef5077b950c4990
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2022-04-21 18:16:54 -0300

    mongo config

---
+++ api/src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const router = Router();
+
+// Importar los routers;
+
+//const tRoute = require('/');
+
+// Configurar los routers
+
+//router.use('/', tRoute);
+
+module.exports = router;
