55a39b68cf81066f1a330b3d5bc703e267dc85bb
caroleoni <carolinaleoni106@gmail.com>
2022-04-26 14:23:23 -0300

    history/back/models: add user.controllers

--- api/src/controllers/users.controllers.js
+++ api/src/controllers/users.controllers.js
@@ -100,30 +100,9 @@ const updateUser =  (req, res) => {
           .then(result => {
               response.json(result)
           })
+          .catch(e => console.log(e))
 }
        
-
-    
-    // try {
-    //     if(!req?.body?.id) {
-    //         console.log(req.body.id)
-    //         return res.status(404).json({error: 'ID parameter is required'});
-    //     }
-    //     const user = await User.findOne({id: req.body.id}).exec(); //para ejecutar una consulta en un modelo "asincrónico"
-    //     if(!user) {
-    //         return res.status(404).json(`No user matches ID ${req.body.id}`)
-    //     }
-    //     if(req.body?.username) user.username = req.body.username
-    //     if(req.body?.firstName) user.firstName = req.body.firstName
-    //     if(req.body?.lastName) user.lastName = req.body.lastName
-    //     if(req.body?.image) user.image = req.body.image
-    //     if(req.body?.favorite) user.favorite = req.body.favorite
-    //     if(req.body?.description) user.description = req.body.description
-    //     res.json(data.user)
-
-    // } catch (error) {
-    //     res.status(404).json({error: 'could not be modified'})
-    // }
 
 const deleteUser = async (req, res) => {
     const { id } = req.params
