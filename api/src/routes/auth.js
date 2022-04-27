9bc34fbb217d8856f68f602e728c7e5776fa8292
Nacho-Thrasher <nachoburgos1995@gmail.com>
2022-04-27 18:39:47 -0300

    history/back/auth add(renew-token, signing-google) agregue sign in google, renew token

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { check } = require('express-validator')
 //? controllers 
-const { login, googleSignIn } = require("../controllers/auth.controllers");
+const { login, googleSignIn, renewToken } = require("../controllers/auth.controllers");
 const { validarCampos } = require('../middlewares/validar.campos.js');
+const { validarJwt } = require('../middlewares/validarJwt');
 
 router.post('/',
     [
@@ -12,6 +13,10 @@ router.post('/',
     ],
     login
 );
-router.post('/google', googleSignIn);
+router.post('/google',  
+    googleSignIn
+);
+router.get('/renew', validarJwt, renewToken)
+
 
 module.exports = router;
