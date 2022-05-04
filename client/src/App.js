473ceaabec759fc5adce8003ada6f06ecfbf092a
cesargaleano <tocega@yahoo.es>
2022-05-04 12:24:55 -0500

    Ajuste ruta Details

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,15 @@ import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
 import { ViewUser } from './components/ViewUser/ViewUser.jsx';
 import Login from './components/Auth/Login.jsx';
-import { DetailsNft } from './components/Home/elements/DetailsNft.jsx';
+//import { DetailsNft } from './components/Home/elements/DetailsNft.jsx';
+import {Details} from './components/Details/Details.jsx';
 import {AllNft} from './components/Home/elements/AllNft.jsx';
 // import PrivateRoute from './services/getPrivateRoute'
 import { Register } from './components/Register/Register.jsx'
 import './App.css';
 import { MisPublicaciones } from "./components/ViewUser/Publicaciones/MisPublicaciones.jsx";
 import { useSelector } from "react-redux";
+
 function MainLayout() {
   return (
     <div>
@@ -37,7 +39,7 @@ function App() {
           <Route element={<MainLayout />}>
             <Route exact path={"/myprofile"} element={logged ? <ViewUser /> : <Home />} />
             <Route exact path={"/myprofile/mispublicaciones"} element={logged ? <MisPublicaciones /> : <Home />} />
-            <Route exact path={"/details/:idNft"} element={<DetailsNft />} />
+            <Route exact path={"/details/:idNft"} element={<Details />} />
             <Route exact path={"/nft"} element={<AllNft />} />
             <Route exact path={"/home"} element={<Home />} />
             <Route exact path={"/home/login"} element={!logged ? <Login /> : <Home/>} />
