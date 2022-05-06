51e8d3c9e4d82d7ec8b955b0e716df57addf9a64
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2022-05-06 14:27:57 -0300

    ultimas correcciones

--- api/src/controllers/transactions.controllers.js
+++ api/src/controllers/transactions.controllers.js
@@ -2,9 +2,21 @@ const Transactions = require('../models/Transactions');
 
 const getAllTransactions = async (req, res) => {
     try {
-        
+        const getTrans = await Transactions.find()
+            .populate("userId", {_id:1}) 
+            .populate("usename", {username:1, _id:0})
+            .populate("nftId", {_id:1})
+            .populate("namenft", {name:1, _id:0})
+            .populate("currencies", {name:1, _id:0})
+
+
+        res.status(200).json(getTrans);
     } catch (error) {
-        
+        res.status(404).json({
+            ok: false,
+            msg: "Transactions not Found"
+        });
+        console.log(error);
     }
 
 };
@@ -12,11 +24,29 @@ const getAllTransactions = async (req, res) => {
 const getTransByNftName = async (req, res) => {
     const { name } = req.query;
     try {
-        
+
     } catch (error) {
-        
+
     }
 
 };
 
-module.exports = { getAllTransactions };
+const createTransaction = async (req, res) => {
+    try {
+        const newTransaction = new Transactions(req.body);
+        await newTransaction.save();
+        res.status(200).json({
+            ok: true,
+            msg: 'Transaction created',
+            newCategory
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: "Transaction could not be created"
+        });
+        console.log(error);
+    };
+};
+
+module.exports = { getAllTransactions, createTransaction };
---
+++ api/src/routes/transaction.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const {createTransaction} = require('../controllers/transactions.controllers');
+
+router.post('/', createTransaction );
+
+module.exports = router;
